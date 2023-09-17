77249a03c9347552d12c288486a0a6e6c6052fda
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-09-17 17:39:09 -0500

    Adding pages

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header/Header"
+import Image from 'next/image';
+
+
+const home = () => {
+  return (
+    <>
+    <div>This is HOME</div>
+    <Header />
+    </> 
+  )
+}
+
+export default home

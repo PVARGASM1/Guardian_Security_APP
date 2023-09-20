2d9b3e4a4d3a058147467244d5f9e9c80d30bc06
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-09-19 21:22:18 -0500

    Adding header

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,10 @@
-import BannerHome from '@/components/Banner';
 import Header from '@components/Header/index';
-import Footer from '@/components/Footer';
-
 
 const HomePage = () => {
   return (
     <div>
     <Header />
-    <BannerHome />
-    <Footer />
+    
     </div> 
   )
 }

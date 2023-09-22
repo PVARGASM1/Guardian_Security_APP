aeb3a8fac61fbe963c1c505adc49da104f150422
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-09-21 22:33:10 -0500

    Adding page login and styles

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,87 +1,92 @@
-import { FaFacebookF, FaGoogle } from 'react-icons/fa';
+import Image from "next/image"
+import Link from "next/link"
 import styles from './Login.module.css'
-
-const handleClickSubmit = (e) => {
-    e.prevent.default
-}
-
-const handleLoginForm = () => {
-  const credentials = { 
-    email: '',
-    password: '',
-  }
-};
 
 const Login = () => {
   return (
-    <div>
-        <form onSubmit={handleClickSubmit}>
+  <div className={styles.background}>
+    <div className={styles.card}>
+    
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-4 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <Image 
+              className="mx-auto w-45 h-40 w-auto"
+              src={'/LogoSinBack.png'}
+              alt='logoGS'
+              width={170}
+              height={170}
+            />
+          <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Ingresa a tu cuenta
+          </h2>
+        </div>
 
-          <h4 className={styles.sign}>Sign in to account</h4>
-          <p className={styles.p}>Enter your email & password to login</p>
-          <div className="form-group">
-            <label htmlFor="sign-in-email">Email Address</label>
-            <br />
-            <input
-              onChange={handleLoginForm}
-              type="email"
-              id="sign-in-email"
-              name="email"
-              required
-              placeholder="guardian.security@test.com"
-            />
-          </div>
-          <div className="form-group password-group">
-            <label htmlFor="sign-in-password">Password</label>
-            <br />
-            <input
-              onChange={handleLoginForm}
-              type="password"
-              id="sign-in-password"
-              name="password"
-              required
-              placeholder="***************"
-            />
-          </div>
-          <div className="form-group">
-            <div className="forgot">
-              {/* <p onClick={handleForgotPassword}> */}
-                Forgot password?
-              {/* </p> */}
-            </div>
-            <div className="btn">
-              <button className="signin-btn" type="submit">Sign In</button>
-            </div>
-          </div>
-          <h6 className="text-muted">Or Sign In With</h6>
-          <div className="social">
-            <div className="social-showcase">
-              <div className="btn-light facebook">
-                <div className="facebook">
-                  <FaFacebookF />
-                </div>
-                <span>Facebook</span>
-              </div>
-              <div className="btn-light google">
-                <div className="google">
-                  <FaGoogle />
-                </div>
-                <span>Google</span>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder=" test@guardian.com"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
             </div>
-          </div>
-          <p className="text-center">
-            Don't have account?
-            <span 
-              // onClick={handleShowAccess}
-            >
-              Create Account
-            </span>
+
+            <div>
+              
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder=" *******"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Contraseña
+                </label>
+                <div className="text-sm">
+                  <Link href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Olvidaste tu contraseña?
+                  </Link>
+                </div>
+              </div>
+
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Ingresa
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            <Link href={'/resgister'} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              ¿No tienes cuenta?
+            </Link>
           </p>
-        </form>
-
+        </div>
+    </div>
 
     </div>
+  </div>
   )
 }
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,9 @@
 import Login from '@components/Login'
+import styles from './login.module.css'
 
 const LoginPage = () => {
     return (
-      <div>
+      <div className={styles.container_login}>
         <Login />
       </div> 
     )
